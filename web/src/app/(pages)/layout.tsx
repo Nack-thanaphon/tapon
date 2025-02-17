2ef4ca8cb966a9ab01e1b6a175a9b966f2e355f4
thanaphon <e21bvz@gmail.com>
2025-02-17 22:20:12 +0700

    update : google analytics

--- web/src/app/(pages)/layout.tsx
+++ web/src/app/(pages)/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import "@/app/front-end.css";
 import ReactQueryProvider from "../shared/react-query/provider";
@@ -9,10 +9,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ReactQueryProvider >
+    <ReactQueryProvider>
       <html lang="en">
         <head>
           <link rel="shortcut icon" href="/vercel.svg" />
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-B9CN5S7MBH"></script>
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-B9CN5S7MBH');
+            `
+          }} />
         </head>
         <body>
           {children}
